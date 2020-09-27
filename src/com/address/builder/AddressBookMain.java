bde157693a0f05b8c0dc73eb778ea2e8d020ed55
avnish986 <avnishgupta986@gmail.com>
2020-09-27 19:38:32 +0530

    [Avnish]Added:uc2 ability to add details using classes contactdetails and addressbook

--- src/com/address/builder/AddressBookMain.java
+++ src/com/address/builder/AddressBookMain.java
@@ -9,6 +9,47 @@ public class AddressBookMain {
 		System.out.println("Welcome to the Address Book");
 		ContactDetails newPerson = new ContactDetails();
 		
+		AddressBook person = new AddressBook();
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter 1 to Add Person details ");
+		int choice = sc.nextInt();
+		Scanner sc1 = new Scanner(System.in); 
+		switch(choice)
+		{
+		case 1:
+			System.out.print("\nEnter Firstname: ");
+	        newPerson.setFname(sc1.nextLine());
+	        
+	        System.out.print("Enter Lastname: ");
+	        newPerson.setLname(sc1.nextLine());
+
+	        System.out.print("Enter Address: ");
+	        newPerson.setAddress(sc1.nextLine());
+
+	        System.out.print("Enter City: ");
+	        newPerson.setCity(sc1.nextLine());
+
+	        System.out.print("Enter State: ");
+	        newPerson.setState(sc1.nextLine());
+
+	        System.out.print("Enter Zip: ");
+	        newPerson.setZip(sc1.nextLine());
+
+	        System.out.print("Enter Phone Number: ");
+	        newPerson.setPhone(sc1.nextLine());
+	        
+	        System.out.print("Enter Email: ");
+	        newPerson.setEmail(sc1.nextLine());
+	        
+	        person.addContact(newPerson);
+	        System.out.println("person details added successfully");
+	        System.out.println(person.getAddressBook());
+	        
+	        break;
+	        
+	        default:
+	        	System.out.println("Enter correct value");
+		}
 	}
 
 }
